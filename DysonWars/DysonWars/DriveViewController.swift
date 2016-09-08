a7cf6226ab05ef99578e0a2e8e47324908199ca8
Nick Everitt <nicholaseveritt@gmail.com>
2016-09-08 14:48:36 +0100

    Added Drive Scene for joystick

--- DysonWars/DysonWars/DriveViewController.swift
+++ DysonWars/DysonWars/DriveViewController.swift
@@ -8,9 +8,45 @@
 
 import Foundation
 import UIKit
+import SpriteKit
 
 class DriveViewController : UIViewController {
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Configure the view.
+        let scene = GameScene(size: self.view.bounds.size)
+        scene.backgroundColor = .white()
+        if let skView = self.view as? SKView {
+            
+            skView.showsFPS = true
+            skView.showsNodeCount = true
+            /* Sprite Kit applies additional optimizations to improve rendering performance */
+            skView.ignoresSiblingOrder = true
+            /* Set the scale mode to scale to fit the window */
+            //scene.scaleMode = .AspectFill
+            skView.presentScene(scene)
+        }
+    }
     
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
     
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask  {
+        if UIDevice.current().userInterfaceIdiom == .phone {
+            return UIInterfaceOrientationMask.allButUpsideDown
+        } else {
+            return UIInterfaceOrientationMask.all
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
 }
